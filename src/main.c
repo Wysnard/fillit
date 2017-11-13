88f0a4ebf6c4940b6fbe8660408d930eb5549a94
Wysnard <Wysnard@users.noreply.github.com>
2017-11-13 02:38:16 +0100

    try

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@ int	main(int argc, char **argv)
 	t_list	*list;
 	char	map[14];
 	int	fd;
-	//t_etris	*tetris;
+	t_etris	*tetris;
 
 	list = NULL;
 	if (argc == 2)
@@ -15,8 +15,8 @@ int	main(int argc, char **argv)
 		ft_bzero(map, 14);
 		if (!(ft_read(fd, list)))
 			ft_putstr_fd("Error\n", 2);
-		//tetris = (t_etris *)list->content;
-		//ft_bitaddtotab(map, tetris, 0);
+		tetris = (t_etris *)list->content;
+		ft_bitaddtotab(map, tetris, 0);
 		//ft_print_bits(*map, 8);
 		//ft_lstdelall(&list);
 	}
