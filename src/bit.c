4bb1a162f526a7fd76bea93651c0c20a0a9692e0
Wysnard <Wysnard@users.noreply.github.com>
2017-11-13 02:51:53 +0100

    try

--- src/bit.c
+++ src/bit.c
@@ -3,9 +3,9 @@
 void	ft_bitaddtotab(char *map, short unsigned bit, size_t at)
 {
 	if (bit)
-		map[at / 8] |= 1 << (8 - at);
+		map[at / 8] |= 1 << (8 - (at - 1));
 	else
-		map[at / 8] &= ~(1 << (8 - at));
+		map[at / 8] &= ~(1 << (8 - (at - 1)));
 }
 
 unsigned short	ft_registerbits(char *buf)
