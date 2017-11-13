ae2b907c3788af14c52eb3578fdf1d39adc7831d
Wysnard <Wysnard@users.noreply.github.com>
2017-11-13 03:31:16 +0100

    re

--- src/main.c
+++ src/main.c
@@ -21,7 +21,7 @@ int	main(int argc, char **argv)
 		//map[0] = map[0] | place;
 		//ft_placetetris(map, tetris->tetrimino, 0);
 		//ft_bitaddtotab(map, 1, 0);
-		ft_print_bits(map[1], 8);
+		ft_print_bits(map[0], 8);
 		//ft_lstdelall(&list);
 	}
 	return (1);
