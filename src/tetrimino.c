05da87779b567b7500e17b3e61dfef8f67d475e0
Vincent LAY <vlay@e1r3p18.42.fr>
2017-11-19 19:06:39 +0100

    uscopy le diable

--- src/tetrimino.c
+++ src/tetrimino.c
@@ -10,51 +10,6 @@ void	ft_bitaddtotab(unsigned short *map, unsigned short int tetrimino, size_t at
 		map[at / 16] &= ~(FIRSTBIT >> (at & 15));
 }
 
-int	ft_mapmove(unsigned short *map, unsigned short tetrimino, size_t at)
-{
-	size_t	i;
-	size_t	rot;
-
-	i = 0;
-	rot = 0;
-	(void)at;
-	while (i < 4)
-	{
-		if (at % 2 < 8)
-		{
-			map[at / 16] = ft_bitrotr(map[at / 16], at & 15);
-			rot = 1;
-		}
-		else
-		{
-			map[at / 16] = ft_bitrotl(map[at / 16], at & 15);
-			rot = 0;
-		}
-		map[i] |= tetrimino & 0xE000;
-		tetrimino <<= 3;
-		i++;
-		at += 16;
-	}
-	return (rot);
-}
-
-void	ft_mapremove(unsigned short *map, size_t at, int rot)
-{
-	size_t	i;
-
-	i = 0;
-	if (rot)
-	{
-		map[at / 16] = ft_bitrotl(map[at / 16], at & 15);
-		rot = 1;
-	}
-	else
-	{
-		map[at / 16] = ft_bitrotr(map[at / 16], at & 15);
-		rot = 0;
-	}
-}
-
 void	ft_placetetris(unsigned short *map, unsigned char tet, unsigned char at)
 {
 	unsigned short	tet_inline;
