18e2f09d33a0b9538f5ff3da62e8460c14a186fc
Vincent LAY <vlay@e1r2p6.42.fr>
2017-11-25 22:03:10 +0100

    maj

--- src/tetriminos_forms.c
+++ src/tetriminos_forms.c
@@ -6,46 +6,47 @@
 /*   By: dsaadia <dsaadia@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/18 15:17:06 by dsaadia           #+#    #+#             */
-/*   Updated: 2017/11/25 21:05:04 by vlay             ###   ########.fr       */
+/*   Updated: 2017/11/25 21:59:55 by vlay             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 #include <stdio.h>
 
-int	ft_isdwrtet(unsigned char tet)
+int					ft_isdwrtet(unsigned char tet)
 {
 	return (tet >> 6 == 0);
 }
 
-int	ft_isuprtet(unsigned char tet)
+int					ft_isuprtet(unsigned char tet)
 {
 	return (tet >> 6 == 1);
 }
 
-int	ft_issqrtet(unsigned char tet)
+int					ft_issqrtet(unsigned char tet)
 {
 	return (tet >> 6 == 3);
 }
 
-int	ft_isdwbtet(unsigned char tet)
+int					ft_isdwbtet(unsigned char tet)
 {
 	return (tet >> 5 == 4);
 }
 
-int	ft_isupbtet(unsigned char tet)
+int					ft_isupbtet(unsigned char tet)
 {
 	return (tet >> 5 == 5);
 }
 
-int	ft_hlassign(unsigned char *h, unsigned char *l, char val_h, char val_l)
+int					ft_hlassign(unsigned char *h,
+		unsigned char *l, char val_h, char val_l)
 {
 	*h = val_h;
 	*l = val_l;
 	return (1);
 }
 
-unsigned short ft_getlinemask(char l)
+unsigned	short	ft_getlinemask(char l)
 {
 	unsigned short	mask;
 	mask = 0;
